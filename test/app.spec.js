54bb465f5108751b82dff5de88e15d571f0de62e
kishorranga <kishor.ranga@gmail.com>
2020-04-03 15:46:57 +0530

    new test cases with mocha

---
+++ test/app.spec.js
@@ -0,0 +1,13 @@
+const mocha = require("mocha");
+const chai = require("chai");
+
+describe('Sample test case', () => {
+    
+    it('testing first case', ()=>{
+        
+        let a = 10;
+        chai.expect(a).equal(10)
+    })
+
+
+});
